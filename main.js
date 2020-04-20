80eefa948ae836d175c6825053e5755f9f8bec22
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-20 20:37:05 +0200

    finito

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 //chiedo all'utente quanti km vuole percorrere e quanti anni ha
 var km = prompt("Quanti km vuoi percorrere?");
 var age = prompt("Quanti anni hai?");
-//imposto il prezzo del biglietto
-var price = (km * 0.21);
-console.log(price);
-
-//imposto lo sconto per passeggero minorenne
-var discountUnderAge = (price * 20) / 100;
-console.log(discountUnderAge);
-//imposto lo sconto per passeggero anziano
-var discountOldAge = (price * 40) / 100;
-console.log(discountOldAge);
-
-
-
-if (age < 18) {
-    console.log(price - discountUnderAge);
-    document.writeln(price - discountUnderAge);
-} else if (age > 65) {
-    console.log(price - discountUnderAge);
-    document.writeln(price - discountOldAge);
+if (isNaN(km) || isNaN(age)) {
+    console.log('deve essere un numero');
+    document.writeln('deve essere un numero');
 } else {
-    document.writeln(price);
+    //imposto il prezzo del biglietto
+    var price = (km * 0.21);
+    console.log(price);
+    //imposto lo sconto per passeggero minorenne
+    var discountUnderAge = (price * 20) / 100;
+    console.log(discountUnderAge);
+    //imposto lo sconto per passeggero anziano
+    var discountOldAge = (price * 40) / 100;
+    console.log(discountOldAge);
+
+    if (age < 18) {
+        console.log(price - discountUnderAge);
+        document.writeln(price - discountUnderAge);
+    } else if (age > 65) {
+        console.log(price - discountUnderAge);
+        document.writeln(price - discountOldAge);
+    } else {
+        document.writeln(price);
+    }
+
 }
